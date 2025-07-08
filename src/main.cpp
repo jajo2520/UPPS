daf1e6ee0a5077f858bfa4c9af02a9972864300e
jajo2520 <eneedley@gmail.com>
2025-07-08 17:46:16 +0200

    Window

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,67 @@
 #include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height); 
+void processInput(GLFWwindow* window);
 
 int main()
 {
-    std::cout << "Hello UPPS!";
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // apple thing
+  
+    GLFWwindow* window = glfwCreateWindow(800, 600, "UPPS", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to initialise window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window); // make main context of current thread
+
+    // deal with glad (load function pointers)
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) // todo learn what this line means ;)
+    {
+        std::cout << "Failed to initialise GLAD" << std::endl;
+        return -1;
+    }
+    
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // tell OpenGL how big our GLFW window is 
+    glViewport(0, 0, 800, 600); // first two is loc of bottom left of window
+
+    // RENDER LOOP
+    while (!glfwWindowShouldClose(window))
+    {
+        processInput(window);
+
+
+        glClearColor(0.2f, 0.2f, 0.2f, 1);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+
+    glfwTerminate();
+
     return 0;
 }
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}  
+
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
